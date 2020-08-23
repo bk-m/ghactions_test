e8c503a743cd6661bb70f2b47e248c92d3cab32c
bk-m <kolja.mielsch@gmail.com>
2020-08-23 19:01:08 +0200

    alsdjf

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world. BLABLA... Going to wait...");
+    println!("MUUUUH *** Hello, world. BLABLA... Going to wait...");
     io::stdin().read_line(&mut String::new()).unwrap();
 }
